1e098ec39e3a2734af9f33d03ee1c5e6cbbce204
Yunpeng Niu <yunpeng@YunpengdeMacBook-Pro.local>
2018-01-15 09:08:03 +0800

    Add my first playground and update git ignore rules

---
+++ MyFirstPlayground.playground/Contents.swift
@@ -0,0 +1,20 @@
+var myVariable = 12
+myVariable = 42
+
+let myConstant: Double = 30
+let myDouble = 70.0
+let myAnotherConstant: Float = 4
+
+let myString = "The length of this table is "
+let myConversion = myString + String(myDouble)
+print(myConversion)
+
+let myAnotherString = "I have a table with the length of \(myConstant) cm."
+print(myAnotherString)
+print("Thus, I have \(1 + 1) tables in total.")
+
+let multipleLines = """
+I have an apple.
+I have a pineapple.
+I have my CS3217 journey.
+"""
