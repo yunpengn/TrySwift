ad0d81ecc72d89ecb3dacc03ac0541637cdca8df
Yunpeng Niu <neilniuyunpeng@gmail.com>
2018-01-15 14:33:57 +0800

    Add some more codes, include if-else, loop

--- MyFirstPlayground.playground/Contents.swift
+++ MyFirstPlayground.playground/Contents.swift
@@ -18,3 +18,27 @@ I have an apple.
 I have a pineapple.
 I have my CS3217 journey.
 """
+
+var myList = ["Apple", "Banana", "Tomato"]
+myList[1] = "Potato"
+
+var jobs = ["Martin": "Professor", "Denna": "Student"]
+jobs["Mark"] = "Human Resources Manager"
+
+let myEmptyArray = [String]()
+let myEmptyDictionary = [String: Float]()
+
+// We are going to explore things about control flow in Swift now.
+// Let's start with if-else statement.
+
+if myVariable > 10 {
+    print("Yes, congratulations!")
+} else {
+    print("Work a bit harder, please.")
+}
+
+for fruit in myList {
+    print("I have a(n) \(fruit).")
+}
+
+
